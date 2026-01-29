9906e98d2b7abee11a048280ca4218d4b17bebf2
Peter Thompson <icarus@Peters-Mac-Studio.local>
2026-01-28 22:46:11 -0800

    Updates

--- _next/static/TUzLBQA9U-oWLOPBVu1SA/_buildManifest.js
+++ _next/static/TUzLBQA9U-oWLOPBVu1SA/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/peter-thompson-portfolio//_next/:path+",
+        "destination": "/peter-thompson-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
